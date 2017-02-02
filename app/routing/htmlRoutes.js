62a9ef800b60e04b18418dd911aa78445545d872
Matthew Tutak <mtutak91@gmail.com>
2017-02-01 23:58:15 -0600

    heroku error

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,33 +1,33 @@
 // *********************************************************************************
 // html-routes.js - this file offers a set of routes for sending users to the various html pages
 // *********************************************************************************
-
 // Dependencies
 // =============================================================
-
 var path = require('path');
-
 // Routes
 // =============================================================
 module.exports = function (app) {
-
     app.get('/test', function (req, res) {
-       res.render("index");
+        res.render("index");
     });
     app.get("/create", function (req, res) {
-    res.sendFile(path.join(__dirname + "/../public/newCreateList.html"));
-    console.log(__dirname);
-	});
-	app.get("/user", function (req, res) {
-    res.sendFile(path.join(__dirname + "/../public/profile.html"));
-    console.log(__dirname);
-	});
-	app.get("/new_list", function (req, res) {
-    res.sendFile(path.join(__dirname + "/../public/listRender.html"));
-    console.log(__dirname);
-	});
+        res.sendFile(path.join(__dirname + "/../public/newCreateList.html"));
+        console.log(__dirname);
+    });
+    app.get("/user", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/profile.html"));
+        console.log(__dirname);
+    });
+    app.get("/new_list", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/listRender.html"));
+        console.log(__dirname);
+    });
     // Default to login
     app.use(function (req, res) {
         res.sendFile(path.join(__dirname + '/../public/welcome.html'));
+    });
+    app.post("/user", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/profile.html"));
+        console.log(__dirname);
     });
 };
